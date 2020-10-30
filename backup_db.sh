00ef739aaabdfc747ea52a272917f59fd0f7d1b1
Julian <48117933+jolution@users.noreply.github.com>
2020-10-30 12:26:28 +0100

    Update backup_db.sh

--- backup_db.sh
+++ backup_db.sh
@@ -11,22 +11,6 @@
 # v0.1
 # Alpha Version, not for production systems
 
-# keep one daily backup for each of the last 7 days
-# At 04:00
-# 0 4   *   *   *   /backup/backup_db.sh daily
-
-# keep one weekly backup for each of the last 4 weeks
-# At 05:00 on Sunday.
-# 0 5 * * 0 /backup/backup_db.sh weekly
-
-# keep one monthly backup for each of the last 6 months
-# At 03:00 on day-of-month 1
-# 0 3 1 * * /backup/backup_db.sh monthly
-
-# keep one yearly backup for each year
-# At 02:00 on day-of-month 1 in January
-# 0 2 1 1 * /backup/backup_db.sh yearly
-
 if [ $# -lt 1 ]; then
 	echo "usage: $0 <daily|weekly|monthly|yearly>"
 	exit 1
